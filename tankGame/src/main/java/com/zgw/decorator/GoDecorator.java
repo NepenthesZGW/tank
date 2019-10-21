f9843383ec34965c9cdc5b452080463bec342573
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-21 15:58:37 +0800

    Create GoDecorator.java

---
+++ tankGame/src/main/java/com/zgw/decorator/GoDecorator.java
@@ -0,0 +1,16 @@
+package com.zgw.decorator;
+
+
+import com.zgw.Facede.GameObject;
+
+import java.awt.*;
+
+public  class GoDecorator extends GameObject {
+    public GameObject gameObject;
+    public GoDecorator(GameObject gameObject){
+        this.gameObject=gameObject;
+    }
+    public void paint(Graphics g) {
+        gameObject.paint(g);
+    }
+}
