5b4357b8424f274ef10a29b807b8eea849dc34f4
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-21 15:59:51 +0800

    Create Wall.java

---
+++ tankGame/src/main/java/com/zgw/tankFrame/Wall.java
@@ -0,0 +1,24 @@
+package com.zgw.tankFrame;
+
+import com.zgw.Facede.GameModel;
+import com.zgw.factory.BaseWall;
+
+import java.awt.*;
+
+public class Wall  extends BaseWall {
+
+
+    public Wall(int x,int y,int width,int height){
+        this.rectangle=new Rectangle(x,y,width,height);
+        GameModel.getInstance().addGameObject(this);
+    }
+
+
+
+    public void paint(Graphics g) {
+        Color oldColor = g.getColor();
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(rectangle.x,rectangle.y,rectangle.width,rectangle.height);
+        g.setColor(oldColor);
+    }
+}
