b9eefc9114eb9cd5e6bbdf991b3ab3b1c17ca499
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-21 15:58:52 +0800

    Create BaseWall.java

---
+++ tankGame/src/main/java/com/zgw/factory/BaseWall.java
@@ -0,0 +1,11 @@
+package com.zgw.factory;
+
+import com.zgw.Facede.GameObject;
+
+import java.awt.*;
+
+public abstract class BaseWall extends GameObject {
+
+
+    public abstract void paint(Graphics g);
+}
