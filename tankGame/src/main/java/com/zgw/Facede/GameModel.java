a78f21fd8cb49260340091a5f25f7d26b5d28b93
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-25 20:37:49 +0800

    Update GameModel.java

--- tankGame/src/main/java/com/zgw/Facede/GameModel.java
+++ tankGame/src/main/java/com/zgw/Facede/GameModel.java
@@ -8,18 +8,19 @@ import com.zgw.strategy.FireStrategy;
 import com.zgw.tankFrame.GoodTank;
 import java.awt.*;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
 
 public class GameModel  {
 
 
     public GoodTank myTank;
-    public List<GameObject> gameObjectList=null;
+    public Map<UUID,GameObject> gameMap=null;
+    public Map<UUID,GameObject> tankMap=null;
     public transient GameFactory gameFactory=null;
     public transient  FireStrategy fireStrategy=null;
     public transient ColliderChain colliderChain=null;
+
 
     private static final GameModel instance=new GameModel();
     static {
@@ -30,7 +31,8 @@ public class GameModel  {
     }
 
     private GameModel(){
-        gameObjectList=new ArrayList<GameObject>();
+        gameMap=new HashMap<UUID, GameObject>();
+        tankMap=new HashMap<UUID, GameObject>();
         colliderChain=new ColliderChain();
         try {
         //jdk 9这个方法被废
@@ -48,58 +50,77 @@ public class GameModel  {
             System.out.println("配置文件加载失败");
     }
     }
+
+
     public void init(){
-        myTank=new GoodTank(200,200,40,40);
-        int enemyCount=Integer.parseInt((String)PropertyMgr.get("initTankCount"));
         Random random = new Random();
+        myTank=new GoodTank(random.nextInt(700),random.nextInt(500),40,40);
+        /*int enemyCount=Integer.parseInt((String)PropertyMgr.get("initTankCount"));
         for (int i = 0; i <enemyCount ; i++) {
             gameFactory.createTank(random.nextInt(800),
                     random.nextInt(300),50,50);
-        }
-
-        int wallCount=Integer.parseInt((String)PropertyMgr.get("wallCount"));
+        }*/
+        /*int wallCount=Integer.parseInt((String)PropertyMgr.get("wallCount"));
         for (int i = 0; i <wallCount ; i++) {
             gameFactory.createWall(random.nextInt(800),
                     random.nextInt(600),50,150);
-        }
+        }*/
     }
 
-    public void createEnemy(){
+    /*public void createEnemy(){
         gameFactory.createTank(new Random().nextInt(800),
                 new Random().nextInt(300),50,50);
-    }
+    }*/
+
 
     public void addGameObject(GameObject gameObject){
-        this.gameObjectList.add(gameObject);
+       if ((gameObject instanceof BaseTank) && !tankMap.containsKey(gameObject.getUuid()))
+           tankMap.put(gameObject.getUuid(), gameObject);
+       this.gameMap.put(gameObject.getUuid(),gameObject);
     }
     public void removeGameObject(GameObject gameObject){
-        this.gameObjectList.remove(gameObject);
+       if((gameObject instanceof BaseTank)&&tankMap.containsKey(gameObject.getUuid()))
+           tankMap.remove(gameObject.getUuid());
+       this.gameMap.remove(gameObject.getUuid());
     }
 
+    public GameObject removeGameObject(UUID uuid){
+        GameObject remove = tankMap.remove(uuid);
+        GameObject remove1 = gameMap.remove(uuid);
+        return remove==remove1?remove:null;
+    }
     public void paint(Graphics g) {
-        myTank.paint(g);
         Color color = g.getColor();
         g.setColor(Color.WHITE);
-        g.drawString("总共的游戏物体数量"+gameObjectList.size(),10,40);
+        g.drawString("总共的游戏物体数量"+gameMap.size(),10,40);
         g.setColor(color);
-        for (int i = 0; i <gameObjectList.size() ; i++) {
-            gameObjectList.get(i).paint(g);
+
+
+        Object[] objects = gameMap.values().toArray();
+        for (int i = 0; i <objects.length ; i++) {
+            ((GameObject)objects[i]).paint(g);
         }
+
         collision();
     }
 
     private void collision(){
-        for (int i = 0; i <gameObjectList.size() ; i++) {
-            for (int j = i+1; j <gameObjectList.size() ; j++) {
-                colliderChain.collision(gameObjectList.get(i),gameObjectList.get(j));
 
+        Object[] objects = gameMap.values().toArray();
+        for (int i = 0; i <objects.length ; i++) {
+            for (int j = i+1; j <objects.length ; j++) {
+                colliderChain.collision((GameObject) objects[i],(GameObject)objects[j]);
             }
         }
 
     }
 
-    public BaseTank getMyTank() {
+    public boolean findTank(UUID id){
+        if(this.tankMap.containsKey(id)) return true;
+        else return  false;
+    }
 
+    public BaseTank getMyTank() {
         return myTank;
     }
 }
