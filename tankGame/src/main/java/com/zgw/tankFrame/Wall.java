1db4f9b0db65cf94d7528cbd11b10c973e73988c
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-25 20:38:28 +0800

    Update Wall.java

--- tankGame/src/main/java/com/zgw/tankFrame/Wall.java
+++ tankGame/src/main/java/com/zgw/tankFrame/Wall.java
@@ -4,12 +4,14 @@ import com.zgw.Facede.GameModel;
 import com.zgw.factory.BaseWall;
 
 import java.awt.*;
+import java.util.UUID;
 
 public class Wall  extends BaseWall {
 
 
     public Wall(int x,int y,int width,int height){
         this.rectangle=new Rectangle(x,y,width,height);
+        this.uuid=UUID.randomUUID();
         GameModel.getInstance().addGameObject(this);
     }
 
