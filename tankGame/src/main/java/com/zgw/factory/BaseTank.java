1f1bd50bdfb6e5efa27fab615274d7e96507f1d6
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-25 20:37:58 +0800

    Update BaseTank.java

--- tankGame/src/main/java/com/zgw/factory/BaseTank.java
+++ tankGame/src/main/java/com/zgw/factory/BaseTank.java
@@ -5,16 +5,23 @@ import com.zgw.strategy.FireStrategy;
 import com.zgw.tankFrame.Direct;
 
 import java.awt.*;
+import java.util.UUID;
 
 public abstract class BaseTank extends GameObject {
     protected Direct direct;
     protected BaseTank(){
         direct=Direct.UP;
     }
-    protected boolean isMoving=false;
+    public boolean isMoving=false;
+    public boolean isMoving() {
+        return isMoving;
+    }
     public Direct getDirect() {
         return direct;
     }
+
+
+
     public void setDirect(Direct direct)
     {
         isMoving=true;
