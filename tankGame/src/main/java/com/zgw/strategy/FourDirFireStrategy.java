ecf66f2c37c9c5a0ff1d5cbc1d04f5a5fe76301f
Nepenthes <52492798+NepenthesZGW@users.noreply.github.com>
2019-10-25 20:38:12 +0800

    Update FourDirFireStrategy.java

--- tankGame/src/main/java/com/zgw/strategy/FourDirFireStrategy.java
+++ tankGame/src/main/java/com/zgw/strategy/FourDirFireStrategy.java
@@ -1,7 +1,10 @@
 package com.zgw.strategy;
 
 import com.zgw.Facede.GameModel;
+import com.zgw.encoder.BulletMsg;
+import com.zgw.factory.BaseBullet;
 import com.zgw.factory.BaseTank;
+import com.zgw.netty.TankClient;
 import com.zgw.tankFrame.Bullet;
 import com.zgw.tankFrame.Direct;
 import com.zgw.tankFrame.MainFrame;
@@ -14,9 +17,14 @@ public class FourDirFireStrategy implements FireStrategy<BaseTank>{
         Rectangle rectangle = tank.getRectangle();
         int bx = rectangle.x + (rectangle.width >> 1);
         int by = rectangle.y + (rectangle.height >> 1);
-        GameModel.getInstance().gameFactory.createBullet(bx - (Bullet.width >> 1),by-(Bullet.height >> 1),Direct.UP,tank);
-        GameModel.getInstance().gameFactory.createBullet(bx - (Bullet.width >> 1),by-(Bullet.height >> 1),Direct.DOWN,tank);
-        GameModel.getInstance().gameFactory.createBullet(bx- (Bullet.height >> 1),by- (Bullet.width >> 1),Direct.LEFT,tank);
-        GameModel.getInstance().gameFactory.createBullet(bx- (Bullet.height >> 1),by- (Bullet.width >> 1),Direct.RIGHT,tank);
+        BaseBullet bullet = GameModel.getInstance().gameFactory.createBullet(bx - (Bullet.width >> 1), by - (Bullet.height >> 1), Direct.UP, tank);
+        TankClient.INSTANCE.send(new BulletMsg(bullet));
+        bullet= GameModel.getInstance().gameFactory.createBullet(bx - (Bullet.width >> 1),by-(Bullet.height >> 1),Direct.DOWN,tank);
+        TankClient.INSTANCE.send(new BulletMsg(bullet));
+        bullet=  GameModel.getInstance().gameFactory.createBullet(bx- (Bullet.height >> 1),by- (Bullet.width >> 1),Direct.LEFT,tank);
+        TankClient.INSTANCE.send(new BulletMsg(bullet));
+        bullet=  GameModel.getInstance().gameFactory.createBullet(bx- (Bullet.height >> 1),by- (Bullet.width >> 1),Direct.RIGHT,tank);
+        TankClient.INSTANCE.send(new BulletMsg(bullet));
+
     }
 }
